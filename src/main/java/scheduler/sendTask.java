8e77166954f1a45b508203db3126058dd5f9c990
MuhammadFauzandotexe <mfauzan51087@gmail.com>
2023-02-28 23:38:28 +0700

    scheduler4

--- src/main/java/scheduler/sendTask.java
+++ src/main/java/scheduler/sendTask.java
@@ -15,13 +15,18 @@ public class sendTask {
     public void everySecond(){
         Integer hours = java.time.LocalTime.now().getHour();
         Integer minute = java.time.LocalTime.now().getMinute();
+        Integer dayOfMonth = java.time.LocalDate.now().getDayOfMonth();
+        Integer month = java.time.LocalDate.now().getMonthValue();
+
+        String date = "2023-02-28";
+        String time = "21:22:00";
         Connection connectionSelect;
         Connection connectionDelete;
         List<String> id= new ArrayList<>();
         List<String> task = new ArrayList<>();
         List<String> taskName = new ArrayList<>();
         try {
-            String statement = "SELECT * FROM user_data where date='2023-02-28' and time='21:14:00' order by id asc limit 1";
+            String statement = "SELECT * FROM user_data where date='"+date+"' and time='"+time+"' order by id asc limit 1";
             connectionSelect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/kawahedukasi","postgres","1");
             PreparedStatement preparedStatement = connectionSelect.prepareStatement(statement);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -30,6 +35,12 @@ public class sendTask {
                 task.add(resultSet.getString("task"));
                 taskName.add(resultSet.getString("task_name"));
             }
+            if(!id.isEmpty()){
+                String statement2 = "DELETE FROM user_data WHERE id = "+id.get(0);
+                connectionDelete = DriverManager.getConnection("jdbc:postgresql://localhost:5432/kawahedukasi","postgres","1");
+                PreparedStatement preparedStatement1 = connectionDelete.prepareStatement(statement2);
+                System.out.println(preparedStatement1.executeUpdate());
+            }else {Log.info("no task");}
         }catch (Exception e){e.printStackTrace();}
         Log.info(id+":"+taskName+":"+task);
     }
